cc02adf334c3d364aab7cb14680529f06207efad
Kylan Wong <mynameiskylan@gmail.com>
2020-10-15 11:37:15 -0700

    add search functionality

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,5 +2,13 @@ class FlightsController < ApplicationController
   def index
     @airports = Airport.all
     @dates = Flight.order(:departure_time).pluck('distinct date(departure_time)')
+
+    @search_results = Flight.search(search_params) if params[:search]
+  end
+
+  private
+
+  def search_params
+    params.require(:search).permit(:departure_airport, :destination_airport, :num_tickets, :departure_date)
   end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,12 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: 'Airport', foreign_key: 'departure_airport_id'
   belongs_to :destination_airport, class_name: 'Airport', foreign_key: 'destination_airport_id'
+
+  def self.search(params)
+    Flight.includes(:departure_airport, :destination_airport).where(
+      departure_airport_id: params[:departure_airport],
+      destination_airport_id: params[:destination_airport],
+      departure_time: params[:departure_date].to_date.all_day
+    )
+  end
 end
